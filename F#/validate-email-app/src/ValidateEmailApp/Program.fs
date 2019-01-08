4bcdb44a8f9897268e02666cd6788ac1eba9023a
Petr Chromec <mortal_flesh@seznam.cz>
2019-01-08 16:22:18 +0100

    Add console style

--- F#/validate-email-app/src/ValidateEmailApp/Program.fs
+++ F#/validate-email-app/src/ValidateEmailApp/Program.fs
@@ -1,8 +1,10 @@
 // Learn more about F# at http://fsharp.org
 
 open System
+open MF.ConsoleStyle
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    Console.title "Hello from Validate E-mail App"
+
     0 // return an integer exit code
