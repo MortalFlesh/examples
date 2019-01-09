54baefbc1ef120cf6055bbee999627db0b052ca7
Petr Chromec <mortal_flesh@seznam.cz>
2019-01-09 14:23:23 +0100

    Add some crazy operators for category php example

--- PHP/category/category.php
+++ PHP/category/category.php
@@ -81,6 +81,37 @@ class PersonWrapper implements Functor, Monad
             ? $this
             : new self($f($this->person));
     }
+
+    public function run(string $operator, callable $f)
+    {
+        switch ($operator) {
+            case '<?>':
+                return $this->map($f);
+            case '>>=':
+                return $this->bind($f);
+            default:
+                return $this;
+        }
+    }
+
+    public function __invoke()
+    {
+        return $this->run(...func_get_args());
+    }
+
+    public function getMap()
+    {
+        return function ($f) {
+            return $this->map($f);
+        };
+    }
+
+    public function getBind()
+    {
+        return function ($f) {
+            return $this->bind($f);
+        };
+    }
 }
 
 $jonSnow = PersonWrapper::loadFromDb(42);
@@ -102,3 +133,32 @@ var_dump($unknown->map($getName));
 echo "\nRename ...\n";
 var_dump($jonSnow->bind($renameTo('Peter Parker'))->map($getName));
 var_dump($unknown->bind($renameTo('Peter Parker'))->map($getName));
+
+echo "\nOperators ...\n";
+${'>>='} = $jonSnow->getBind();
+${'<?>'} = ${'>>='}($renameTo('Peter Parker'))->getMap();
+$name = ${'<?>'}($getName);
+
+var_dump('name:', $name);
+
+echo "\nRun ...\n";
+$name = $jonSnow
+    ->run('>>=', $renameTo('Peter Parker'))
+    ->run('<?>', $getName);
+var_dump($name);
+
+$name = $unknown
+    ->run('>>=', $renameTo('Peter Parker'))
+    ->run('<?>', $getName);
+var_dump($name);
+
+echo "\nChain ...\n";
+$name = $jonSnow
+    ('>>=', $renameTo('Peter Parker'))
+    ('<?>', $getName);
+var_dump($name);
+
+$name = $unknown
+    ('>>=', $renameTo('Peter Parker'))
+    ('<?>', $getName);
+var_dump($name);
